c946335b9fc24726597205102ca507cf8ea324e8
ngtiendaz <datnguyentien433@gmail.com>
2026-03-25 14:49:42 +0700

    Initial Commit

---
+++ ZingMP3Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZingMP3Demo
+//
+//  Created by Daz on 25/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZingMP3Demo/ZingMP3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZingMP3DemoApp.swift
+//  ZingMP3Demo
+//
+//  Created by Daz on 25/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ZingMP3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
